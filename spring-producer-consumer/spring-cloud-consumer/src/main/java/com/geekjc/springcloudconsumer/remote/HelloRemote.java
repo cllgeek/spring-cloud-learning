47cb5e597cb7c8608fbb8e5cd827427f960c4272
howard <jshoward@itcom888.com>
2019-10-07 11:17:19 +0800

    add spring-producer-consumer

---
+++ spring-producer-consumer/spring-cloud-consumer/src/main/java/com/geekjc/springcloudconsumer/remote/HelloRemote.java
@@ -0,0 +1,15 @@
+package com.geekjc.springcloudconsumer.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ll
+ * @date 2019年10月07日 11:06 AM
+ */
+@FeignClient(name = "spring-cloud-producer")
+public interface HelloRemote {
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
